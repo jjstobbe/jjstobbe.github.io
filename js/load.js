a6bbc59fe548c1534330d67dcf51b2461bca2afb
John Stobbe <jjstobbe@gmail.com>
2016-06-02 20:33:47 -0500

    Redesigned experience page, also changed header animations
    I will probably edit the experience page more, I'm still not that
    satisfied with how it looks.

--- js/load.js
+++ js/load.js
@@ -41,34 +41,55 @@ $(window).scroll(function(){
 $(document).ready(function (){
     $("#Hexagons ul li").click(function(){
         var $target = $(this);
-        if(!$(this).hasClass("current")){
-            $(this).addClass("current");
+        /*Select a hexagon when no other hexagon is selected*/
 
+        if(!$(this).hasClass("current") && !$('#Hexagons ul li:not(:nth-child('+($target.index()+1)+')').hasClass("notCurrent")){
+            $(this).addClass("current");
+            $('#Hexagons ul li:not(:nth-child('+($target.index()+1)+')').addClass("notCurrent");
             /*Adds a delay to the show*/
-            setTimeout(function(){
-                $('#Hexagons ul li:not(:nth-child('+($target.index()+1)+')').hide();
-                $('#Hexagons span').show();
-                $('#Hexagons span').addClass("animated slideInRight");
-                $('#Hexagons span').css('display','block');
-            }, 500);
-            $('#Hexagons ul li:not(:nth-child('+($(this).index()+1)+')').addClass("animated fadeOut");
+            $('#Hexagons ul li:not(:nth-child('+($target.index()+1)+')').animate({opacity: 0.5});
 
             /*Adds delay for content*/
             setTimeout(function(){
                 $('#content li:nth-child('+($target.index()+1)+')').show();
+                $('#content li:nth-child(8)').show();
+                $('#content li:nth-child('+($target.index()+9)+')').show();
             }, 500);
 
             $('#content li:nth-child('+($(this).index()+1)+')').addClass("animated slideInLeft");
+            $('#content li:nth-child(8)').addClass("animated fadeIn");
+            $('#content li:nth-child('+($(this).index()+9)+')').addClass("animated slideInRight");
+            $('html, body').animate({scrollTop: $("#lower").offset().top}, 2000);
 
 
-        }else{
-            $('#Hexagons ul li:not(:nth-child('+($(this).index()+1)+')').removeClass("animated fadeOut");
-            $('#Hexagons ul li:not(:nth-child('+($(this).index()+1)+')').addClass("animated fadeIn");
-            $('#Hexagons ul li:not(:nth-child('+($(this).index()+1)+')').show();
+        }else if($(this).hasClass("current")){
+            $('#Hexagons ul li:not(:nth-child('+($target.index()+1)+')').animate({opacity: 1});
+
             $(this).removeClass("current");
-            $('#Hexagons span').hide();
+            $('#Hexagons ul li:not(:nth-child('+($target.index()+1)+')').removeClass("notCurrent");
 
-            $('#content li:nth-child('+($(this).index()+1)+')').hide();
+
+
+
+            setTimeout(function(){
+                $('#content li:nth-child('+($target.index()+1)+')').removeClass("animated slideOutRight");
+                $('#content li:nth-child(8)').removeClass("animated fadeOut");
+                $('#content li:nth-child('+($target.index()+9)+')').removeClass("animated slideOutLeft");
+
+                $('#content li:nth-child('+($target.index()+1)+')').hide();
+                $('#content li:nth-child(8)').hide();
+                $('#content li:nth-child('+($target.index()+9)+')').hide();
+            }, 500);
+
+            /*Transition out animations*/
+            $('#content li:nth-child(8)').removeClass("animated fadeIn");
+            $('#content li:nth-child(8)').addClass("animated fadeOut");
+
+            $('#content li:nth-child('+($(this).index()+1)+')').removeClass("animated slideInLeft");
+            $('#content li:nth-child('+($(this).index()+1)+')').addClass("animated slideOutRight");
+
+            $('#content li:nth-child('+($(this).index()+9)+')').removeClass("animated slideInRight");
+            $('#content li:nth-child('+($(this).index()+9)+')').addClass("animated slideOutLeft");
         }
     });
 });
