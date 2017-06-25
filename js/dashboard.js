b545bafcfaef14d84c9bb38ea36a586a8b92005e
John Stobbe <jjstobbe@gmail.com>
2017-06-25 17:14:42 -0500

    Made generic ajax method

--- js/dashboard.js
+++ js/dashboard.js
@@ -13,7 +13,7 @@ function ToDoListVM() {
     self.GetToDos = function() {
         self.ToDoList.removeAll();
         
-        GetToDos(function(data) {
+        ajax("GET", "https://baas.kinvey.com/appdata/kid_BJFBIVmX-/ToDo", null, function(data) {
             $.each(data, function(index, value){
                 self.ToDoList.push(new ToDo(value._id, value.Content, value.Completed, value.Time, value.Order));
             });
@@ -45,57 +45,19 @@ function ToDo(id, content, completed, time, order) {
         self.Order = ToDoListVM.ToDoList().length + 1;
         var dataObject = ko.toJSON(self);
         
-        $.ajax
-        ({
-          type: "POST",
-          url: "https://baas.kinvey.com/appdata/kid_BJFBIVmX-/ToDo",
-          contentType: 'application/json; charset=utf-8',
-          data: dataObject,
-          headers: {
-            "Authorization": "Kinvey " + document.cookie.substring(document.cookie.indexOf("=")+1),
-              "X-Kinvey-API-Version": "3"
-          },success: function(data){
+        ajax("POST", "https://baas.kinvey.com/appdata/kid_BJFBIVmX-/ToDo", dataObject, function(data){
               $("#textBox").val("");
               ToDoListVM.ToDoList.push(new ToDo(data._id, data.Content, data.Completed, data.Time, data.Order));
               var cols = document.querySelectorAll('#ToDoList .ToDoElement');
               [].forEach.call(cols, addDnDHandlers);
-          },error: function(data){
-              // Redirects if authentication fails
-              window.location.href = "/login"
-          }
         });
     };
 };
 
-function GetToDos(handleData) {
-    // Makes API call to get the list of ToDos
-    $.ajax
-    ({
-      type: "GET",
-      url: "https://baas.kinvey.com/appdata/kid_BJFBIVmX-/ToDo",
-      dataType: 'json',
-      headers: {
-        "Authorization": "Kinvey " + document.cookie.substring(document.cookie.indexOf("=")+1)
-      },success: function(data){
-          handleData(data);
-      },error: function(data){
-          // Redirects if authentication fails
-          window.location.href = "/login"
-      }
-    });
-}
-
 function Logout() {
-    $.ajax
-    ({
-      type: "POST",
-      url: "https://baas.kinvey.com/user/kid_BJFBIVmX-/_logout",
-      headers: {
-        "Authorization": "Kinvey " + document.cookie.substring(document.cookie.indexOf("=")+1)
-      },success: function(data){
-          document.cookie = "authToken=; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
-          window.location.href="/login";
-      }
+    ajax("POST", "https://baas.kinvey.com/user/kid_BJFBIVmX-/_logout", null, function() {
+      document.cookie = "authToken=; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+      window.location.href="/login";
     });
 }
 
@@ -107,44 +69,21 @@ function checkSubmit(e) {
 }
 
 function moveToDo(dragId, baseId){
-    console.log(dragId, baseId);
-    console.log(ToDoListVM.ToDoList());
-    
     var counter = 1;
     $('.ToDoElement').each(function() {
         var self = this;
         var id = $(this).children()[0].id;
         if(self.id != counter){
             var position = counter;
-            $.ajax
-            ({
-              type: "GET",
-              url: "https://baas.kinvey.com/appdata/kid_BJFBIVmX-/ToDo/"+id,
-              dataType: 'json',
-              headers: {
-                "Authorization": "Kinvey " + document.cookie.substring(document.cookie.indexOf("=")+1)
-              },success: function(data){
+            ajax("GET", "https://baas.kinvey.com/appdata/kid_BJFBIVmX-/ToDo/"+id, null,           function(data) {
                   var changedToDo = {
                       Content: data.Content,
                       Completed: data.Completed,
                       Time: data.Time,
                       Order: position
                   }
-
                   var dataObject = ko.toJSON(changedToDo);
-
-                  $.ajax
-                    ({
-                      type: "PUT",
-                      url: "https://baas.kinvey.com/appdata/kid_BJFBIVmX-/ToDo/"+id,
-                      contentType: 'application/json; charset=utf-8',
-                      data: dataObject,
-                      headers: {
-                        "Authorization": "Kinvey " + document.cookie.substring(document.cookie.indexOf("=")+1),
-                          "X-Kinvey-API-Version": "3"
-                      }
-                    });
-                }
+                  ajax("PUT", "https://baas.kinvey.com/appdata/kid_BJFBIVmX-/ToDo/"+id, dataObject, function(){});
             });
         }
         counter+=1;
@@ -158,45 +97,19 @@ function checkEdit(e) {
         var newContent = e.srcElement.innerText.replace(/\r?\n|\r/g, '');
         $("#"+id).blur();
         
-        $.ajax
-        ({
-          type: "GET",
-          url: "https://baas.kinvey.com/appdata/kid_BJFBIVmX-/ToDo/"+id,
-          dataType: 'json',
-          headers: {
-            "Authorization": "Kinvey " + document.cookie.substring(document.cookie.indexOf("=")+1)
-          },success: function(data){
+        ajax("GET", "https://baas.kinvey.com/appdata/kid_BJFBIVmX-/ToDo/"+id, null, 
+        function(data){
               var changedToDo = {
                   Content: newContent,
                   Completed: data.Completed,
                   Time: data.Time,
                   Order: data.Order
               }
-              
-              
-              
               var dataObject = ko.toJSON(changedToDo);
-              
-              $.ajax
-                ({
-                  type: "PUT",
-                  url: "https://baas.kinvey.com/appdata/kid_BJFBIVmX-/ToDo/"+id,
-                  contentType: 'application/json; charset=utf-8',
-                  data: dataObject,
-                  headers: {
-                    "Authorization": "Kinvey " + document.cookie.substring(document.cookie.indexOf("=")+1),
-                      "X-Kinvey-API-Version": "3"
-                  },success: function(data){
-                      notifySuccess("Success", "Task has been updated");
-                  },error: function(data){
-                      // Redirects if authentication fails
-                      window.location.href = "/login"
-                  }
-                });
-          },error: function(data){
-              // Redirects if authentication fails
-              window.location.href = "/login"
-          }
+            
+            ajax("PUT", "https://baas.kinvey.com/appdata/kid_BJFBIVmX-/ToDo/"+id, dataObject, function(data){
+                  notifySuccess("Success", "Task has been updated");
+             });
         });
     }
 }
@@ -213,21 +126,8 @@ function finishToDo() {
     // Adds the id back for later use
     self.Id = ko.observable(id);
     
-    $.ajax
-    ({
-      type: "PUT",
-      url: "https://baas.kinvey.com/appdata/kid_BJFBIVmX-/ToDo/"+id,
-      contentType: 'application/json; charset=utf-8',
-      data: dataObject,
-      headers: {
-        "Authorization": "Kinvey " + document.cookie.substring(document.cookie.indexOf("=")+1),
-          "X-Kinvey-API-Version": "3"
-      },success: function(data){
-          notifySuccess("Success", "Task Completed");
-      },error: function(data){
-          // Redirects if authentication fails
-          window.location.href = "/login.html"
-      }
+    ajax("PUT", "https://baas.kinvey.com/appdata/kid_BJFBIVmX-/ToDo/"+id, dataObject, function(data){
+        notifySuccess("Success", "Task Completed");
     });
 }
 
@@ -236,26 +136,27 @@ function deleteToDo() {
     
     var id = self.Id();
     
+    ajax("DELETE", "https://baas.kinvey.com/appdata/kid_BJFBIVmX-/ToDo/"+id, null, function(data){
+        location.reload();
+    });
+}
+
+/* Generic AJAX call to edit the database */
+function ajax(type, url, data, successFunction) {
     $.ajax
     ({
-      type: "DELETE",
-      url: "https://baas.kinvey.com/appdata/kid_BJFBIVmX-/ToDo/"+id,
+      type: type,
+      url: url,
       contentType: 'application/json; charset=utf-8',
+      dataType: 'json',
+      data: data,
       headers: {
         "Authorization": "Kinvey " + document.cookie.substring(document.cookie.indexOf("=")+1),
           "X-Kinvey-API-Version": "3"
       },success: function(data){
-          location.reload();
+          successFunction(data);
       },error: function(data){
-          //console.log("failed");
           window.location.href = "/login.html"
       }
     });
 }
-
-
-
-
-
-
-
