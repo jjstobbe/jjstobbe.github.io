8fdf59898112a5cad36154803de3f0595184bc5e
John Stobbe <jjstobbe@gmail.com>
2017-06-25 00:13:24 -0500

    Drag and drop, css changes

--- js/dashboard.js
+++ js/dashboard.js
@@ -15,26 +15,35 @@ function ToDoListVM() {
         
         GetToDos(function(data) {
             $.each(data, function(index, value){
-                self.ToDoList.push(new ToDo(value._id, value.Content, value.Completed, value.Time));
+                self.ToDoList.push(new ToDo(value._id, value.Content, value.Completed, value.Time, value.Order));
             });
             
-            var table = $("#toDoTable").DataTable( { "bStateSave": true, "aaSorting": [] } );
+            // Sorts the list by Order
+            self.ToDoList.sort(sortFunction);
             
-            // Hides bottom content of data table
+            var cols = document.querySelectorAll('#ToDoList .ToDoElement');
+            [].forEach.call(cols, addDnDHandlers);
         });
     };
 };
 
-function ToDo(id, content, completed, time) {
+function sortFunction(a, b) {
+    return a.Order()-b.Order();
+}
+
+function ToDo(id, content, completed, time, order) {
     var self = this;
     
     self.Id = ko.observable(id);
     self.Content = ko.observable(content);
     self.Completed = ko.observable(completed);
     self.Time = ko.observable(time);
+    self.Order = ko.observable(order);
     
     self.AddToDo = function() {
         delete self.Id;
+        self.Order = ToDoListVM.ToDoList().length + 1;
+        console.log(self.Order);
         var dataObject = ko.toJSON(self);
         
         $.ajax
@@ -48,7 +57,7 @@ function ToDo(id, content, completed, time) {
               "X-Kinvey-API-Version": "3"
           },success: function(data){
               $("#textBox").val("");
-              ToDoListVM.ToDoList.push(new ToDo(data._id, data.Content, data.Completed, data.Time));
+              ToDoListVM.ToDoList.push(new ToDo(data._id, data.Content, data.Completed, data.Time, data.Order));
               notifySuccess("Success", "Task has been added");
           },error: function(data){
               // Redirects if authentication fails
@@ -97,9 +106,55 @@ function checkSubmit(e) {
     }
 }
 
+function moveToDo(dragId, baseId){
+    console.log(dragId, baseId);
+    console.log(ToDoListVM.ToDoList());
+    
+    var counter = 1;
+    $('.ToDoElement').each(function() {
+        var self = this;
+        var id = $(this).children()[0].id;
+        if(self.id != counter){
+            var position = counter;
+            $.ajax
+            ({
+              type: "GET",
+              url: "https://baas.kinvey.com/appdata/kid_BJFBIVmX-/ToDo/"+id,
+              dataType: 'json',
+              headers: {
+                "Authorization": "Kinvey " + document.cookie.substring(document.cookie.indexOf("=")+1)
+              },success: function(data){
+                  var changedToDo = {
+                      Content: data.Content,
+                      Completed: data.Completed,
+                      Time: data.Time,
+                      Order: position
+                  }
+
+                  var dataObject = ko.toJSON(changedToDo);
+
+                  $.ajax
+                    ({
+                      type: "PUT",
+                      url: "https://baas.kinvey.com/appdata/kid_BJFBIVmX-/ToDo/"+id,
+                      contentType: 'application/json; charset=utf-8',
+                      data: dataObject,
+                      headers: {
+                        "Authorization": "Kinvey " + document.cookie.substring(document.cookie.indexOf("=")+1),
+                          "X-Kinvey-API-Version": "3"
+                      }
+                    });
+                }
+            });
+        }
+        counter+=1;
+    });
+}
+
 function checkEdit(e) {
     if (e && e.keyCode == 13) {
         var id = e.srcElement.id;
+        console.log(e.srcElement.innerText);
         var newContent = e.srcElement.innerText.replace(/\r?\n|\r/g, '');
         $("#"+id).blur();
         
@@ -114,8 +169,11 @@ function checkEdit(e) {
               var changedToDo = {
                   Content: newContent,
                   Completed: data.Completed,
-                  Time: data.Time
+                  Time: data.Time,
+                  Order: data.Order
               }
+              
+              
               
               var dataObject = ko.toJSON(changedToDo);
               
