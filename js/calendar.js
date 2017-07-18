3157fc65f671224b92ed54d22655fc0d480f7516
John Stobbe <jjstobbe@gmail.com>
2017-07-17 20:39:46 -0500

    Fixed all kinds of calendar issues with other years

--- js/calendar.js
+++ js/calendar.js
@@ -15,22 +15,23 @@ $(document).ready(function() {
 
 function renderCal(themonth) {
     $('.calendar li').remove();
-    $('.calendar ul').append('<li>Mo</li><li>Tu</li><li>We</li><li>Th</li><li>Fr</li><li>Sa</li> <li>Su</li>');
+    $('.calendar ul').append('<li>Mo</li><li>Tu</li><li>We</li><li>Th</li><li>Fr</li><li>Sa</li><li>Su</li>');
     var d = new Date(),
-        currentMonth = d.getMonth() + themonth, // get this month
-        days = numDays(currentMonth, d.getYear()), // get number of days in the month
-        fDay = firstDay(currentMonth, d.getYear()) - 1, // find what day of the week the 1st lands on
+        currentMonth = mod((d.getMonth() + themonth-1),12),
+        currentYear = (1900 + d.getYear() + Math.floor((d.getMonth()+themonth - 1)/12)),// get this year
+        days = numDays(currentMonth, currentYear), // get number of days in the month
+        fDay = firstDay(currentMonth, currentYear), // find what day of the week the 1st lands on
         months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']; // month names
-
-    $('.calendar p.monthname').html(months[currentMonth - 1] +" &nbsp;  "+(1900 + d.getYear())); // add month name to calendar
+    
+    $('.calendar p.monthname').html(months[currentMonth] +" &nbsp;  "+currentYear); // add month name to calendar
 
     for (var i = 0; i < fDay - 1; i++) { // place the first day of the month in the correct position
         $('<li>&nbsp;</li>').appendTo('.calendar ul');
     }
     
-    console.log(d.getDate());
     for (var i = 1; i <= days; i++) { // write out the days
-        if(d.getDate() == i){
+        // Adds a 'selected' class to the current day
+        if(themonth == 1 && d.getDate() == i && 1900+d.getYear() == currentYear){
             $('<li class="selected">' + i + '</li>').appendTo('.calendar ul');
         }else {
             $('<li>' + i + '</li>').appendTo('.calendar ul');
@@ -42,6 +43,10 @@ function renderCal(themonth) {
     }
 
     function numDays(month, year) {
-        return new Date(year, month, 0).getDate();
+        return new Date(year, month+1, 0).getDate();
+    }
+    
+    function mod(n, m) {
+        return ((n % m) + m) % m;
     }
 }
