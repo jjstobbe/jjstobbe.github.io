187343796a64cf4aca01b61e7820a2346df04cfb
John Stobbe <jjstobbe@gmail.com>
2017-09-12 13:26:54 -0500

    Updated publish.sh

--- publish.sh
+++ publish.sh
@@ -3,7 +3,8 @@ webpack -p
 git add dist
 git commit -m "$desc"
 git subtree push --prefix dist origin master
-echo "Done."
+echo "Done uploading to master."
 git add .
 git commit -m "$desc"
 git push
+echo "Done pushing to dev."
